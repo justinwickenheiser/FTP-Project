2ec0afd03592df6e6ef440714cf43b5f72c22bd0
Justin Wickenheiser <Justin@rapinbe1f8j7.office.ads.gvsu.edu>
2016-10-04 16:52:32 -0400

    Modified how the offset is handled by using the revcMsgSize variable

--- ftp_client.java
+++ ftp_client.java
@@ -3,76 +3,77 @@ import java.net.* ;
 import java.util.* ;
 
 public class ftp_client {
-    
-    public static void main(String[] args) throws IOException {
-        int controlPort = 2264;
-        int dataPort = 2265;
-        String server = args[0];       // Server name or IP address
-        String cmd = new String("");
-        Scanner console = new Scanner(System.in);
-        boolean timeToQuit = false;
-        
-        if (args.length != 1)  // Test for correct # of args
-            throw new IllegalArgumentException("Parameter(s): <Server>");
-        
-        // Create socket that is connected to server on specified port
-        Socket controlConnection = new Socket(server, controlPort);
-        System.out.println("Connected to server...CONTROLLER");
-        
-        InputStream inFromServer_Control = controlConnection.getInputStream();
-        OutputStream outToServer_Control = controlConnection.getOutputStream();
-        
-        System.out.print("cmd: ");
-        cmd = console.next();
-        
-        // Establish the listen socket.
-        ServerSocket dataListen = new ServerSocket(dataPort);
-        
-        while (!timeToQuit) {
-            
-            outToServer_Control.write(cmd.getBytes());
-            
-            // Listen for a TCP connection request.
-            Socket dataConnection = dataListen.accept();    // This socket goes with ServerSocket
-            
-            InputStream inFromServer_Data = dataConnection.getInputStream();
-            OutputStream outToServer_Data = dataConnection.getOutputStream();
-            
-            System.out.println("Connected to server...DATA LINE");
-            
-            
-            // close dataConnection
-            dataConnection.close();
-            System.out.println("Client DATA LINE Closed");
-            
-            // if quiting, then indicate so; else, get next command.
-            if (cmd.toLowerCase().equals("quit")) {
-                timeToQuit = true;
-            } else {
-                System.out.print("cmd: ");
-                cmd = console.next();
-            }
-        }
-        
-        /*
-        InputStream in = socket.getInputStream();
-        OutputStream out = socket.getOutputStream();
-        
-        out.write(byteBuffer);  // Send the encoded string to the server
-        
-        // Receive the same string back from the server
-        int totalBytesRcvd = 0;  // Total bytes received so far
-        int bytesRcvd;           // Bytes received in last read
-        while (totalBytesRcvd < byteBuffer.length) {
-            if ((bytesRcvd = in.read(byteBuffer, totalBytesRcvd,
-                                     byteBuffer.length - totalBytesRcvd)) == -1)
-                throw new SocketException("Connection close prematurely");
-            totalBytesRcvd += bytesRcvd;
-        }
-        
-        System.out.println("Received: " + new String(byteBuffer));
-        */
-        controlConnection.close();  // Close the socket and its streams
-        System.out.println("Client CONTROLLER Closed");
-    }
+	
+	public static void main(String[] args) throws IOException {
+		int controlPort = 2264;
+		int dataPort = 2265;
+		String server = args[0];	   // Server name or IP address
+		String cmd = new String("");
+		Scanner console = new Scanner(System.in);
+		boolean timeToQuit = false;
+		
+		if (args.length != 1)  // Test for correct # of args
+			throw new IllegalArgumentException("Parameter(s): <Server>");
+		
+		// Create socket that is connected to server on specified port
+		Socket controlConnection = new Socket(server, controlPort);
+		System.out.println("Connected to server...CONTROLLER");
+		System.out.println("");
+		
+		InputStream inFromServer_Control = controlConnection.getInputStream();
+		OutputStream outToServer_Control = controlConnection.getOutputStream();
+		
+		System.out.print("cmd: ");
+		cmd = console.next();
+		
+		// Establish the listen socket.
+		ServerSocket dataListen = new ServerSocket(dataPort);
+		
+		while (!timeToQuit) {
+			
+			outToServer_Control.write(cmd.getBytes());
+			
+			// Listen for a TCP connection request.
+			Socket dataConnection = dataListen.accept();	// This socket goes with ServerSocket
+			
+			InputStream inFromServer_Data = dataConnection.getInputStream();
+			OutputStream outToServer_Data = dataConnection.getOutputStream();
+			
+			System.out.println("Connected to server...DATA LINE");
+			
+			// close dataConnection
+			dataConnection.close();
+			System.out.println("Client DATA LINE Closed");
+			System.out.println("");
+			
+			// if quiting, then indicate so; else, get next command.
+			if (cmd.toLowerCase().equals("quit")) {
+				timeToQuit = true;
+			} else {
+				System.out.print("cmd: ");
+				cmd = console.next();
+			}
+		}
+		
+		/*
+		InputStream in = socket.getInputStream();
+		OutputStream out = socket.getOutputStream();
+		
+		out.write(byteBuffer);  // Send the encoded string to the server
+		
+		// Receive the same string back from the server
+		int totalBytesRcvd = 0;  // Total bytes received so far
+		int bytesRcvd;		   // Bytes received in last read
+		while (totalBytesRcvd < byteBuffer.length) {
+			if ((bytesRcvd = in.read(byteBuffer, totalBytesRcvd,
+									 byteBuffer.length - totalBytesRcvd)) == -1)
+				throw new SocketException("Connection close prematurely");
+			totalBytesRcvd += bytesRcvd;
+		}
+		
+		System.out.println("Received: " + new String(byteBuffer));
+		*/
+		controlConnection.close();  // Close the socket and its streams
+		System.out.println("Client CONTROLLER Closed");
+	}
 }
--- ftp_server.java
+++ ftp_server.java
@@ -3,86 +3,89 @@ import java.net.* ;
 import java.util.* ;
 
 public class ftp_server {
-    private static final int BUFSIZE = 256;   // Size of receive buffer
-    
-    public static void main(String argv[]) throws Exception {
-        int controlPort = 2264;
-        int dataPort = 2265;
-        int recvMsgSize;   // Size of received message
-        byte[] byteBuffer = new byte[BUFSIZE];  // Receive buffer
-        
-        // Establish the listen socket.
-        ServerSocket controlListen = new ServerSocket(controlPort);
-        
-        // Process HTTP service requests in an infinite loop.
-        while (true) {
-            // Listen for a TCP connection request.
-            Socket controlConnection = controlListen.accept();
-            
-            InputStream inFromClient_Control = controlConnection.getInputStream();
-            OutputStream outToClient_Control = controlConnection.getOutputStream();
-            
-            
-            System.out.println("Client connected to Server: CONTROLLER");
-            
-            // Receive until client closes connection, indicated by -1 return
-            while ((recvMsgSize = inFromClient_Control.read(byteBuffer)) != -1) {
-                // create cmdReceived
-                String cmdReceived = new String(byteBuffer,0,4);
-                
-                
-                // open dataConnection socket
-                Socket dataConnection = new Socket(controlConnection.getInetAddress(), dataPort);
-                System.out.println("Connected to client...DATA LINE");
-                
-                System.out.println("Received: " + cmdReceived.toLowerCase());
-                
-                /*
-                 * IF STATEMENTS NOT WORKING!!!!
-                 * Sort of. I had to declare cmdReceived using an offset of 4 bytes since
-                 * there are 4 letters each in LIST, RETR, STOR, QUIT
-                 */
-                
-                // based on cmdReceived do something
-                if (cmdReceived.toLowerCase().equals("list")) {
-                    
-                    System.out.println("Running " + cmdReceived);
-                    
-                } else if (cmdReceived.toLowerCase().equals("retr")) {
-                    
-                    System.out.println("Running " + cmdReceived);
-                    
-                } else if (cmdReceived.toLowerCase().equals("stor")) {
-                    
-                    System.out.println("Running " + cmdReceived);
-                    
-                } else if (cmdReceived.toLowerCase().equals("quit")) {
-                    
-                    System.out.println("Running " + cmdReceived);
-                    
-                    dataConnection.close();
-                    System.out.println("Server DATA LINE Closed");
-                    
-                    controlConnection.close();
-                    System.out.println("Server CONTROLLER Closed");
-                    
-                    System.exit(0);
-                }
-                
-                dataConnection.close();
-                System.out.println("Server DATA LINE Closed");
-            }
-            
-            // Construct an object to process the HTTP request message.
-            //HttpRequest request = new HttpRequest(connection);
-            
-            // Create a new thread to process the request.
-            //Thread thread = new Thread(request);
-            
-            // Start the thread.
-            //thread.start();
-        }
-    }
+	private static final int BUFSIZE = 256;   // Size of receive buffer
+	
+	public static void main(String argv[]) throws Exception {
+		int controlPort = 2264;
+		int dataPort = 2265;
+		int recvMsgSize;   // Size of received message
+		byte[] byteBuffer = new byte[BUFSIZE];  // Receive buffer
+		
+		// Establish the listen socket.
+		ServerSocket controlListen = new ServerSocket(controlPort);
+		
+		// Process HTTP service requests in an infinite loop.
+		while (true) {
+			// Listen for a TCP connection request.
+			Socket controlConnection = controlListen.accept();
+			
+			InputStream inFromClient_Control = controlConnection.getInputStream();
+			OutputStream outToClient_Control = controlConnection.getOutputStream();
+			
+			
+			System.out.println("Client connected to Server: CONTROLLER");
+			System.out.println("");
+			
+			// Receive until client closes connection, indicated by -1 return
+			while ((recvMsgSize = inFromClient_Control.read(byteBuffer)) != -1) {
+				// create cmdReceived
+				String cmdReceived = new String(byteBuffer,0,recvMsgSize);
+				
+				
+				// open dataConnection socket
+				Socket dataConnection = new Socket(controlConnection.getInetAddress(), dataPort);
+				System.out.println("Connected to client...DATA LINE");
+				
+				System.out.println("Message Size: " + recvMsgSize);
+				System.out.println("Received: " + cmdReceived.toLowerCase());
+				
+				/*
+				 * IF STATEMENTS NOT WORKING!!!!
+				 * Sort of. I had to declare cmdReceived using an offset of 4 bytes since
+				 * there are 4 letters each in LIST, RETR, STOR, QUIT
+				 */
+				
+				// based on cmdReceived do something
+				if (cmdReceived.toLowerCase().equals("list")) {
+					
+					System.out.println("Running " + cmdReceived);
+					
+				} else if (cmdReceived.toLowerCase().equals("retr")) {
+					
+					System.out.println("Running " + cmdReceived);
+					
+				} else if (cmdReceived.toLowerCase().equals("stor")) {
+					
+					System.out.println("Running " + cmdReceived);
+					
+				} else if (cmdReceived.toLowerCase().equals("quit")) {
+					
+					System.out.println("Running " + cmdReceived);
+					
+					dataConnection.close();
+					System.out.println("Server DATA LINE Closed");
+					
+					controlConnection.close();
+					System.out.println("Server CONTROLLER Closed");
+					
+					System.exit(0);
+				}
+				
+				dataConnection.close();
+				System.out.println("Server DATA LINE Closed");
+				System.out.println("");
+			}
+			
+			// Construct an object to process the HTTP request message.
+			//HttpRequest request = new HttpRequest(connection);
+			
+			// Create a new thread to process the request.
+			//Thread thread = new Thread(request);
+			
+			// Start the thread.
+			//thread.start();
+		}
+	}
 }
 
 
